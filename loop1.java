ceeecc722fdf82e0fff272a4250201882cb1730b
snehat1806-png <snehat1806@gmail.com>
2025-09-01 20:07:48 +0530

    third commit

---
+++ loop1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class loop1 {
+    public static void main(String [] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number");
+    int n=sc.nextInt();
+    for(int i=1;i<n;i++){
+        if(i%2==0){
+            System.out.println(i);
+        }
+
+    } 
+    sc.close();
+}
+}
